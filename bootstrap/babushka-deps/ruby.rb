528a56b32e7473ec399e3ae62fb0ee19dc4688d1
Andrew Kiellor <akiellor@gmail.com>
2013-04-08 16:16:02 -0700

    Added ruby-build for rbenv.

--- bootstrap/babushka-deps/ruby.rb
+++ bootstrap/babushka-deps/ruby.rb
@@ -7,7 +7,13 @@ end
 
 dep "rbenv", :user do
   requires 'rbenv.git'.with(:user => user, :commit => "7a10b64cf7e4df3261dec94f3c609a64a04998ef")
+  requires 'rbenv-ruby-build.git'.with(:user => user, :commit => "5273fb325e642553cdd04b86fce5c9d6c6832367")
+  requires 'make.managed'
   requires 'rbenv.profile'.with(:user => user)
+end
+
+dep 'make.managed' do
+  provides 'make'
 end
 
 dep "rbenv.git", :user, :commit do
@@ -19,6 +25,18 @@ dep "rbenv.git", :user, :commit do
      shell! "git clone git://github.com/sstephenson/rbenv.git #{rbenv}"
      shell! "chown -R #{user} #{rbenv}"
      shell! "cd #{rbenv} && git reset --hard #{commit}"
+  }
+end
+
+dep "rbenv-ruby-build.git", :user, :commit do
+  user_home = Etc.getpwnam(user).dir
+  ruby_build = File.expand_path(File.join(".rbenv", "plugins", "ruby-build"), user_home)
+ 
+  met? { `git --git-dir=\"#{File.join(ruby_build, ".git")}\" log HEAD...HEAD~1 --format=%H`.strip == commit}
+  meet {
+     shell! "git clone git://github.com/sstephenson/ruby-build.git #{ruby_build}"
+     shell! "chown -R #{user} #{ruby_build}"
+     shell! "cd #{ruby_build} && git reset --hard #{commit}"
   }
 end
 
